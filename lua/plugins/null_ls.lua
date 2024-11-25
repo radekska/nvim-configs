1da7172a93399545b4416fe615e7b9a2b5fd6435
Radosław Skałbania <radoslaw@cast.ai>
2024-11-25 08:33:41 +0100

    updates

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -6,7 +6,13 @@ return {
     local sources = {
       null_ls.builtins.formatting.golines.with {
         extra_args = { "-m", "180" },
-      }
+      },
+      null_ls.builtins.diagnostics.sqlfluff.with({
+        extra_args = { "--dialect", "postgres" },
+      }),
+      null_ls.builtins.formatting.sqlfluff.with({
+        extra_args = { "--dialect", "postgres" },
+      }),
     }
     null_ls.setup({ sources = sources })
   end
