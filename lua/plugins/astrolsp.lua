15bbc62e1e32861e5010703ef31ba4cf59986e94
Radosław Skałbania <radoslaw@cast.ai>
2025-03-13 10:52:23 +0100

    fixes updates etc...

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,160 +1,98 @@
 ---@type LazySpec
 return {
-  "AstroNvim/astrolsp",
-  ---@type AstroLSPOpts
-  opts = {
-    autocmds = {
-      yaml_to_helm_detection = {
-        cond = function(_, bufnr)
-          local bufname = vim.api.nvim_buf_get_name(bufnr) -- Get full file path
-          local ext = bufname:match("^.+%.(.+)$")          -- Extract extension
-          if ext == "yaml" or ext == "yml" then
-            return true
-          end
-          return false
-        end,
-        {
-          event = { "BufNewFile", "BufRead", "BufWrite", "BufEnter", "FileReadPre" },
-          callback = function()
-            local bufnr = vim.api.nvim_get_current_buf()
-            local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+	"AstroNvim/astrolsp",
+	---@type AstroLSPOpts
+	opts = {
+		autocmds = {
+			yaml_to_helm_detection = {
+				cond = function(_, bufnr)
+					local bufname = vim.api.nvim_buf_get_name(bufnr) -- Get full file path
+					local ext = bufname:match("^.+%.(.+)$") -- Extract extension
+					if ext == "yaml" or ext == "yml" then
+						return true
+					end
+					return false
+				end,
+				{
+					event = { "BufNewFile", "BufRead", "BufWrite", "BufEnter", "FileReadPre" },
+					callback = function()
+						print("worked")
+						local bufnr = vim.api.nvim_get_current_buf()
+						local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
-            for _, line in ipairs(lines) do
-              if line:match("{{.+}}") then
-                vim.bo[bufnr].filetype = "helm"
-                local clients = vim.lsp.get_active_clients()
-                for _, client in ipairs(clients) do
-                  if client.name == "yamlls" then -- Replace with the LSP name you want to disable
-                    vim.lsp.buf_detach_client(bufnr, client.id)
-                  end
-                end
-                return
-              end
-            end
-          end
-        }
+						for _, line in ipairs(lines) do
+							if line:match("{{.+}}") then
+								vim.bo[bufnr].filetype = "helm"
+								local clients = vim.lsp.get_clients({ bufnr = bufnr })
+								for _, client in ipairs(clients) do
+									if client.name == "yamlls" then -- Replace with the LSP name you want to disable
+										vim.lsp.buf_detach_client(bufnr, client.id)
+									end
+								end
+								return
+							end
+						end
+					end,
+				},
+			},
+		},
+		commands = {
+			FormatBuffer = {
+				function()
+					vim.lsp.buf.format(require("astrolsp").format_opts)
+				end,
+				desc = "Format file with LSP",
+			},
+		},
+		-- Configuration table of features provided by AstroLSP
+		features = {
+			autoformat = false, -- enable or disable auto formatting on start
+			codelens = true, -- enable/disable codelens refresh on start
+			inlay_hints = false, -- enable/disable inlay hints on start
+			semantic_tokens = true, -- enable/disable semantic token highlighting
+		},
+		-- customize lsp formatting options
+		formatting = {
+			async = true, -- enable or disable async formatting
 
-
-      }
-    },
-    commands = {
-      FormatBuffer = {
-        function() vim.lsp.buf.format(require("astrolsp").format_opts) end,
-        desc = "Format file with LSP",
-      },
-    },
-    -- Configuration table of features provided by AstroLSP
-    features = {
-      autoformat = false,     -- enable or disable auto formatting on start
-      codelens = true,        -- enable/disable codelens refresh on start
-      inlay_hints = false,    -- enable/disable inlay hints on start
-      semantic_tokens = true, -- enable/disable semantic token highlighting
-    },
-    -- customize lsp formatting options
-    formatting = {
-      async = true, -- enable or disable async formatting
-
-
-      -- control auto formatting on save
-      format_on_save = {
-        enabled = false, -- enable or disable format on save globally
-        -- allow_filetypes = { -- enable format on save for specified filetypes only
-        --   -- "go",
-        -- },
-        -- ignore_filetypes = { -- disable format on save for specified filetypes
-        --   -- "python",
-        -- },
-      },
-      -- disabled = { -- disable formatting capabilities for the listed language servers
-      --   -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-      --   -- "lua_ls",
-      -- },
-      timeout_ms = 8000, -- default format timeout
-    },
-    -- -- enable servers that you already have installed without mason
-    servers = {
-      "terraformls",
-      "gopls",
-      "starpls",
-      "lua_ls",
-      "yamlls",
-      "marksman",
-      -- "pyright"
-      "djlsp",
-      "cssls",
-      "bufls"
-    },
-    -- -- customize language server configuration options passed to `lspconfig`
-    -- ---@diagnostic disable: missing-fields
-    -- config = {
-    --   -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
-    -- },
-    -- -- customize how language servers are attached
-    handlers = {
-      --   -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts)
-      --   local navbuddy = require("nvim-navbuddy")
-      --   require("lspconfig")[server].setup({ on_attach = function(client, bufnr) navbuddy.attach(client, bufnr) end })
-      -- end
-      --
-      --   -- the key is the server that is being setup with `lspconfig`
-      --   -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
-      --   -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
-    },
-    -- -- Configure buffer local auto commands to add when attaching a language server
-    -- autocmds = {
-    --   -- first key is the `augroup` to add the auto commands to (:h augroup)
-    --   lsp_document_highlight = {
-    --     -- Optional condition to create/delete auto command group
-    --     -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
-    --     -- condition will be resolved for each client on each execution and if it ever fails for all clients,
-    --     -- the auto commands will be deleted for that buffer
-    --     cond = "textDocument/documentHighlight",
-    --     -- cond = function(client, bufnr) return client.name == "lua_ls" end,
-    --     -- list of auto commands to set
-    --     {
-    --       -- events to trigger
-    --       event = { "CursorHold", "CursorHoldI" },
-    --       -- the rest of the autocmd options (:h nvim_create_autocmd)
-    --       desc = "Document Highlighting",
-    --       callback = function() vim.lsp.buf.document_highlight() end,
-    --     },
-    --     {
-    --       event = { "CursorMoved", "CursorMovedI", "BufLeave" },
-    --       desc = "Document Highlighting Clear",
-    --       callback = function() vim.lsp.buf.clear_references() end,
-    --     },
-    --   },
-    -- },
-    --   -- mappings to be set up on attaching of a language server
-    mappings = {
-      n = {
-        ["<Leader>lff"] = {
-          function() vim.lsp.buf.format(require("astrolsp").format_opts) end,
-          desc = "Format buffer",
-        }
-      }
-
-      --     n = {
-      --       gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
-      --       -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
-      --       -- gD = {
-      --       --   function() vim.lsp.buf.declaration() end,
-      --       --   desc = "Declaration of current symbol",
-      --       --   cond = "textDocument/declaration",
-      --       -- },
-      --       -- ["<Leader>uY"] = {
-      --       --   function() require("astrolsp.toggles").buffer_semantic_tokens() end,
-      --       --   desc = "Toggle LSP semantic highlight (buffer)",
-      --       --   cond = function(client) return client.server_capabilities.semanticTokensProvider and vim.lsp.semantic_tokens end,
-      --       -- },
-      --     },
-    },
-    --   -- A custom `on_attach` function to be run after the default `on_attach` function
-    --   -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
-    --   on_attach = function(client, bufnr)
-    --     -- this would disable semanticTokensProvider for all clients
-    --     -- client.server_capabilities.semanticTokensProvider = nil
-    --   end,
-  },
+			-- control auto formatting on save
+			format_on_save = {
+				enabled = false, -- enable or disable format on save globally
+				-- allow_filetypes = { -- enable format on save for specified filetypes only
+				--   -- "go",
+				-- },
+				-- ignore_filetypes = { -- disable format on save for specified filetypes
+				--   -- "python",
+				-- },
+			},
+			-- disabled = { -- disable formatting capabilities for the listed language servers
+			--   -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+			--   -- "lua_ls",
+			-- },
+			timeout_ms = 8000, -- default format timeout
+		},
+		-- -- enable servers that you already have installed without mason
+		servers = {
+			"terraformls",
+			"gopls",
+			"starpls",
+			"lua_ls",
+			"yamlls",
+			"marksman",
+			"djlsp",
+			"cssls",
+			"protols",
+		},
+		handlers = {},
+		mappings = {
+			n = {
+				["<Leader>lff"] = {
+					function()
+						vim.lsp.buf.format(require("astrolsp").format_opts)
+					end,
+					desc = "Format buffer",
+				},
+			},
+		},
+	},
 }
--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function()
-      require("codeium").setup({
-        virtual_text = {
-          enabled = true,
-          key_bindings = {
-            accept = "<C-y>",
-            next = "<C->>",
-            prev = "<C-<>",
-          }
-        },
-      })
-      require('codeium.virtual_text').set_statusbar_refresh(function()
-        require('lualine').refresh()
-      end)
-    end
-  }
+  -- {
+  --   "Exafunction/codeium.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "hrsh7th/nvim-cmp",
+  --   },
+  --   config = function()
+  --     require("codeium").setup({
+  --       virtual_text = {
+  --         enabled = true,
+  --         key_bindings = {
+  --           accept = "<C-y>",
+  --           next = "<C->>",
+  --           prev = "<C-<>",
+  --         }
+  --       },
+  --     })
+  --     require('codeium.virtual_text').set_statusbar_refresh(function()
+  --       require('lualine').refresh()
+  --     end)
+  --   end
+  -- }
 }
