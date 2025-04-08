aec086f1dbf8d07852a45f8e780ef82ae5f52e9e
Radosław Skałbania <radoslaw@cast.ai>
2025-04-08 12:35:51 +0200

    stable

--- lua/plugins/nvim-bqf.lua
+++ lua/plugins/nvim-bqf.lua
@@ -1,6 +1,12 @@
 return {
-  {
-    "kevinhwang91/nvim-bqf",
-    event = "VeryLazy",
-  },
+	{
+		"kevinhwang91/nvim-bqf",
+		event = "VeryLazy",
+		dependencies = {
+			"junegunn/fzf",
+			config = function()
+				vim.fn["fzf#install"]()
+			end,
+		},
+	},
 }
